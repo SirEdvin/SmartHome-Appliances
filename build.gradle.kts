c9a48f5377e15beb79fe9964558a4498e16f6178
Edvin <me@siredvin.site>
2025-03-06 23:39:54 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import java.util.function.BiConsumer
+
+plugins {
+    java
+    id("site.siredvin.root") version "0.8.14"
+    id("site.siredvin.release") version "0.8.14"
+    id("com.dorongold.task-tree") version "2.1.1"
+}
+
+subprojectShaking {
+    withKotlin.set(true)
+}
+
+val setupSubproject = subprojectShaking::setupSubproject
+
+subprojects {
+    setupSubproject(this)
+}
+
+githubShaking {
+    modBranch.set("1.20")
+    projectRepo.set("Template")
+//    mastodonProjectName.set("Template")
+    shake()
+}
+
+repositories {
+    mavenCentral()
+}
