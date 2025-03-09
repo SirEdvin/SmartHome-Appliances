6c2cbe02f4f8bccbebba24a8ca46df2735d9cca9
SirEdvin <me@siredvin.site>
2025-03-10 00:05:38 +0200

    Fighting with rotation

--- projects/core/src/main/kotlin/site/siredvin/smarthome/data/ModBlockModelProvider.kt
+++ projects/core/src/main/kotlin/site/siredvin/smarthome/data/ModBlockModelProvider.kt
@@ -6,6 +6,7 @@ import net.minecraft.data.models.blockstates.MultiVariantGenerator
 import net.minecraft.data.models.blockstates.PropertyDispatch
 import net.minecraft.data.models.blockstates.Variant
 import net.minecraft.data.models.blockstates.VariantProperties
+import net.minecraft.data.models.blockstates.VariantProperties.*
 import net.minecraft.data.models.model.ModelLocationUtils
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.level.block.state.properties.BooleanProperty
@@ -13,6 +14,7 @@ import site.siredvin.smarthome.common.block.LampBlock
 import site.siredvin.smarthome.common.block.LedPanelBlock
 import site.siredvin.smarthome.common.block.SwitchBlock
 import site.siredvin.smarthome.common.setup.Blocks
+import kotlin.math.min
 
 object ModBlockModelProvider {
 
@@ -21,6 +23,14 @@ object ModBlockModelProvider {
         Direction.SOUTH -> VariantProperties.Rotation.R180
         Direction.EAST -> VariantProperties.Rotation.R90
         Direction.WEST -> VariantProperties.Rotation.R270
+        else -> VariantProperties.Rotation.R0
+    }
+
+    private fun toLedPanelRotation(direction: Direction): VariantProperties.Rotation = when (direction) {
+        Direction.SOUTH -> VariantProperties.Rotation.R0
+        Direction.NORTH -> VariantProperties.Rotation.R180
+        Direction.WEST -> VariantProperties.Rotation.R90
+        Direction.EAST -> VariantProperties.Rotation.R270
         else -> VariantProperties.Rotation.R0
     }
 
@@ -87,23 +97,50 @@ object ModBlockModelProvider {
         val offModel =  ModelLocationUtils.getModelLocation(block, "_off")
         val onModelTwoSide =  ModelLocationUtils.getModelLocation(block, "_two_side_on")
         val offModelTwoSide =  ModelLocationUtils.getModelLocation(block, "_two_side_off")
+        val onModelTwoSideSide =  ModelLocationUtils.getModelLocation(block, "_two_side_side_on")
+        val offModelTwoSideSide =  ModelLocationUtils.getModelLocation(block, "_two_side_side_off")
+        val brokenModel =  ModelLocationUtils.getModelLocation(block, "_broken")
 
-        val dispatch = PropertyDispatch.properties(LedPanelBlock.AMOUNT, LedPanelBlock.SECOND_FACING, LedPanelBlock.ENALBED)
+        val dispatch = PropertyDispatch.properties(LedPanelBlock.AMOUNT, LedPanelBlock.FACING, LedPanelBlock.SECOND_FACING, LedPanelBlock.ENALBED)
         // Single panel
-        Direction.entries.forEach {
-            dispatch.select(1, it, false, Variant.variant().with(VariantProperties.MODEL, offModel))
-            dispatch.select(1, it, true, Variant.variant().with(VariantProperties.MODEL, onModel))
+        Direction.entries.forEach { first ->
+            val offVariant = Variant.variant().with(VariantProperties.Y_ROT, toYAnglePedestal(first)).with(VariantProperties.X_ROT, toXAnglePedestal(first)).with(VariantProperties.MODEL, offModel)
+            val onVariant = Variant.variant().with(VariantProperties.Y_ROT, toYAnglePedestal(first)).with(VariantProperties.X_ROT, toXAnglePedestal(first)).with(VariantProperties.MODEL, onModel)
+            Direction.entries.forEach { second ->
+                dispatch.select(1, first, second, false, offVariant)
+                dispatch.select(1, first, second, true, onVariant)
+            }
         }
         // Two panels
-        Direction.entries.forEach {
-            dispatch.select(2, it, false, Variant.variant().with(VariantProperties.MODEL, offModelTwoSide))
-            dispatch.select(2, it, true, Variant.variant().with(VariantProperties.MODEL, onModelTwoSide))
+        Direction.entries.forEach { first ->
+            Direction.entries.forEach { second ->
+                if (first == second || first.opposite == second) { // This in general should not appear, so we consider it broker
+                    dispatch.select(2, first, second, false, Variant.variant().with(VariantProperties.MODEL, brokenModel))
+                    dispatch.select(2, first, second, true, Variant.variant().with(VariantProperties.MODEL, brokenModel))
+                } else if (first == Direction.UP || second == Direction.UP) {
+                    val important = if (first == Direction.UP) second else first
+                    dispatch.select(2, first, second, false, Variant.variant().with(VariantProperties.MODEL, offModelTwoSide).with(VariantProperties.Y_ROT, toYAnglePedestal(important.opposite)))
+                    dispatch.select(2, first, second, true, Variant.variant().with(VariantProperties.MODEL, onModelTwoSide).with(VariantProperties.Y_ROT, toYAnglePedestal(important.opposite)))
+                } else if (first == Direction.DOWN || second == Direction.DOWN) {
+                    val important = if (first == Direction.DOWN) second else first
+                    dispatch.select(2, first, second, false, Variant.variant().with(VariantProperties.MODEL, offModelTwoSide).with(VariantProperties.Y_ROT, toYAnglePedestal(important)).with(VariantProperties.X_ROT, VariantProperties.Rotation.R180))
+                    dispatch.select(2, first, second, true, Variant.variant().with(VariantProperties.MODEL, onModelTwoSide).with(VariantProperties.Y_ROT, toYAnglePedestal(important)).with(VariantProperties.X_ROT, VariantProperties.Rotation.R180))
+                } else {
+                    val firstRotation = toLedPanelRotation(first)
+                    val secondRotation = toLedPanelRotation(second)
+                    val targetRotation = if (firstRotation.ordinal > secondRotation.ordinal) secondRotation else firstRotation
+                    dispatch.select(2, first, second, false, Variant.variant().with(VariantProperties.MODEL, offModelTwoSideSide).with(
+                        X_ROT, targetRotation))
+                    dispatch.select(2, first, second, true, Variant.variant().with(VariantProperties.MODEL, onModelTwoSideSide).with(
+                        X_ROT, targetRotation))
+                }
+            }
         }
         generators.blockStateOutput.accept(
             MultiVariantGenerator.multiVariant(
                 block,
                 Variant.variant().with(VariantProperties.MODEL, onModel),
-            ).with(createFacingDispatch()).with(dispatch),
+            ).with(dispatch),
         )
         generators.delegateItemModel(block, ModelLocationUtils.getModelLocation(block))
     }
